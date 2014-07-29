c63c7fece5f43fc80357c5170c58329c7859e066
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-29 21:20:17 +0000

    v1.0
    git-svn-id: https://plugins.svn.wordpress.org/wpsso-ssb/trunk@956923 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,66 @@
+=== WPSSO Social Sharing Buttons (SSB) ===
+Contributors: jsmoriss
+Donate Link: http://surniaulula.com/extend/plugins/wpsso-buttons/
+Tags: wpsso, like, send, share, facebook, google, google plus, g+, twitter, linkedin, social, pinterest, tumblr, stumbleupon, widget, shortcode, bbpress, buddypress
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.txt
+Requires At Least: 3.0
+Tested Up To: 3.9.1
+Stable Tag: 1.0
+
+Social Sharing Buttons (SSB) extension for the WordPress Social Sharing Optimization (WPSSO) plugin.
+
+== Description ==
+
+The WPSSO Social Sharing Buttons (SSB) plugin works in conjunction with [WPSSO](http://wordpress.org/plugins/wpsso/), extending WPSSO's existing settings with additional pages, tabs, and options with social sharing button features.
+
+These new settings allow you to include a selection of social sharing buttons in several locations:
+
+* Above and/or below your content and/or excerpt text.
+* On admin editing pages, including media, product pages, and custom post types.
+* On [bbPress](http://wordpress.org/plugins/bbpress/) single pages.
+* On [BuddyPress](http://wordpress.org/plugins/buddypress/) activity entries.
+* In a WordPress sharing widget.
+* In a CSS / javascript button sidebar.
+* From a shortcode within your content and/or excerpt.
+* From a function in your theme's template(s).
+
+Each of the following social sharing buttons can be enabled, configured, and styled individually:
+
+* Buffer
+* Facebook (Like, Send, and Share)
+* Google+
+* LinkedIn
+* ManageWP
+* Pinterest
+* Reddit
+* StumbleUpon
+* Tumblr (Links, Quotes, Images, Videos)
+* Twitter (Summary, Large Image Summary, Photo, Gallery, Player and Product Cards)
+
+The Facebook, Google+ and Twitter sharing buttons also support *multiple languages*, and the language can match the webpage language (WordPress locale) dynamically. WPSSO SSB can also include hashtags (from WordPress and/or NextGEN Gallery tag names) in Tweet texts and other social captions.
+
+**Please note that [WordPress Social Sharing Optimization (WPSSO)](http://wordpress.org/plugins/wpsso/) version 2.6+ is required to use this WPSSO extension.**
+
+== Installation ==
+
+== Frequently Asked Questions ==
+
+== Other Notes ==
+
+== Screenshots ==
+
+1. An example showing all social sharing buttons enabled in the CSS sidebar, content, and widget, using the default WPSSO SSB stylesheet.
+2. The extra Sharing Buttons tab added to the Social Settings metabox on admin editing pages.
+
+== Changelog ==
+
+= Version 1.0 =
+
+* Bugfixes
+	* *None*
+* Enhancements
+	* *None*
+
+== Upgrade Notice ==
+
---
+++ wpsso-ssb.php
@@ -0,0 +1,103 @@
+<?php
+/*
+ * Plugin Name: WPSSO Social Sharing Buttons (SSB)
+ * Plugin URI: http://surniaulula.com/extend/plugins/wpsso-ssb/
+ * Author: Jean-Sebastien Morisset
+ * Author URI: http://surniaulula.com/
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl.txt
+ * Description: Social Sharing Buttons (SSB) extension for the WordPress Social Sharing Optimization (WPSSO) plugin.
+ * Requires At Least: 3.0
+ * Tested Up To: 3.9.1
+ * Version: 1.0
+ * 
+ * Copyright 2014 - Jean-Sebastien Morisset - http://surniaulula.com/
+*/
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'These aren\'t the droids you\'re looking for...' );
+
+if ( ! class_exists( 'WpssoSsb' ) ) {
+
+	class WpssoSsb {
+
+		private $plugin_name = 'WPSSO Social Sharing Buttons (SSB)';
+		private $opt_version = 'ssb1';
+		private $min_version = '2.6';
+		private $has_min_ver = true;
+
+		public $p;		// class object variables
+		public $cf = array();	// config array defined in construct method
+
+		public function __construct() {
+			// don't continue if the social sharing buttons are disabled
+			if ( defined( 'WPSSOSSB_SOCIAL_SHARING_DISABLE' ) &&
+				WPSSOSSB_SOCIAL_SHARING_DISABLE )
+					return;
+
+			require_once ( dirname( __FILE__ ).'/lib/config.php' );
+			WpssoSsbConfig::set_constants( __FILE__ );
+			WpssoSsbConfig::require_libs( __FILE__ );
+
+			$this->add_filters();	// hooks filter to extends the wpsso config
+
+			add_action( 'wpsso_init_options', array( &$this, 'init_options' ), 10 );
+			add_action( 'wpsso_init_addon', array( &$this, 'init_addon' ), 10 );
+		}
+
+		// runs at class construct
+		private function add_filters() {
+			$filters_to_add = array( 'get_config' => 1 );
+			foreach ( $filters_to_add as $filter_name => $filter_args )
+				add_filter( 'wpsso_'.$filter_name, array( &$this, 'filter_'.$filter_name ), 10, $filter_args );
+		}
+
+		// this filter is executed at init priority -1
+		public function filter_get_config( $cf ) {
+			if ( version_compare( $cf['plugin']['wpsso']['version'], $this->min_version, '<' ) ) {
+				$this->has_min_ver = false;
+				return $cf;
+			}
+			$cf['opt']['version'] .= $this->opt_version;
+			$cf = SucomUtil::array_merge_recursive_distinct( $cf, WpssoSsbConfig::$cf );
+			return $cf;
+		}
+
+		// this action is executed when WpssoOptions::__construct() is executed (class object is created)
+		public function init_options() {
+			global $wpsso;
+			$this->p =& $wpsso;
+
+			if ( $this->has_min_ver === false )
+				return;
+
+			$this->p->is_avail['ssb'] = true;
+			$this->p->is_avail['admin']['apikeys'] = true;
+			$this->p->is_avail['admin']['sharing'] = true;
+			$this->p->is_avail['admin']['style'] = true;
+			$this->p->is_avail['util']['shorten'] = ( ! empty( $this->p->options['twitter_shortener'] ) && 
+				$this->p->options['twitter_shortener'] !== 'none' ? true : false );
+		}
+
+		// this action is executed once all class objects and addons have been created
+		public function init_addon() {
+			if ( $this->has_min_ver === false ) {
+				$this->p->debug->log( WPSSOSSB_PLUGINBASE.' requires at least WPSSO version '.
+					$this->min_version.' ('.$wpsso_version.' installed)' );
+				if ( is_admin() )
+					$this->p->notice->err( $this->plugin_name.' v'.WpssoSsbConfig::$cf['plugin']['wpssossb']['version'].
+						' requires WPSSO v'.$this->min_version.
+						' or newer (version '.$this->p->cf['plugin']['wpsso']['version'].
+						' is currently installed).', true );
+				return;
+			}
+			WpssoSsbConfig::load_lib( false, 'sharing' );
+			$this->p->sharing = new WpssoSsbSharing( $this->p, __FILE__ );
+		}
+	}
+
+        global $wpssossb;
+	$wpssossb = new WpssoSsb();
+}
+
+?>
