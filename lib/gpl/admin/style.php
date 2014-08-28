24ee6628ed6ae37a4f4330a0b6b0afb157e1a5b9
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-28 00:35:33 +0000

    v1.0.6
    git-svn-id: https://plugins.svn.wordpress.org/wpsso-ssb/trunk@974382 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/gpl/admin/style.php
+++ lib/gpl/admin/style.php
@@ -25,14 +25,14 @@ if ( ! class_exists( 'WpssoSsbGplAdminStyle' ) ) {
 			) );
 		}
 
-		private function filter_style_common_rows( &$rows, &$form, $idx ) {
+		public function filter_style_common_rows( &$rows, &$form, $idx ) {
 			$text = $this->p->msgs->get( 'info-style-'.$idx );
-			if ( ! empty( WpssoSsbSharing::$cf['sharing']['show_on'][$idx] ) ) {
+			if ( isset( $this->p->options['buttons_preset_'.$idx] ) ) {
 				$text .= '<p><strong>The social sharing button options for the '.$idx.
 				' style are subject to preset values, selected on the '.
-				$this->p->util->get_admin_url( 'sharing#sucom-tab_sharing_preset', 'Social Sharing settings page' ).
+				$this->p->util->get_admin_url( 'sharing#sucom-tab_sharing_preset', 'Sharing Buttons settings page' ).
 				', to modify their action (share vs like), size, and counter orientation.</strong> '.
-				'The width and height values in your CSS should relect these presets (if any).</p>';
+				'The width and height values in your CSS should reflect these presets (if any).</p>';
 				$text .= '<p><strong>Selected preset:</strong> '.
 					( empty( $this->p->options['buttons_preset_'.$idx] ) ? '[none]' :
 						$this->p->options['buttons_preset_'.$idx] ).'</p>';
