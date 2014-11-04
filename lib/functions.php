efeeffcaf05d91d92a523ce09937fb9c7f6843d9
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-04 16:56:35 +0000

    v1.0.10.3
    git-svn-id: https://plugins.svn.wordpress.org/wpsso-ssb/tags/1.0.10.3@1019621 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/functions.php
@@ -0,0 +1,42 @@
+<?php
+/*
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.txt
+Copyright 2012-2014 - Jean-Sebastien Morisset - http://surniaulula.com/
+*/
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'These aren\'t the droids you\'re looking for...' );
+
+if ( ! function_exists( 'wpssossb_get_sharing_buttons' ) ) {
+	function wpssossb_get_sharing_buttons( $ids = array(), $atts = array() ) {
+		global $wpsso;
+		if ( $wpsso->is_avail['ssb'] ) {
+			if ( $wpsso->is_avail['cache']['transient'] ) {
+				$cache_salt = __METHOD__.'(lang:'.SucomUtil::get_locale().'_url:'.$wpsso->util->get_sharing_url().
+					'_ids:'.( implode( '_', $ids ) ).'_atts:'.( implode( '_', $atts ) ).')';
+				$cache_id = $wpsso->cf['lca'].'_'.md5( $cache_salt );
+				$cache_type = 'object cache';
+				$wpsso->debug->log( $cache_type.': transient salt '.$cache_salt );
+				$html = get_transient( $cache_id );
+				if ( $html !== false ) {
+					$wpsso->debug->log( $cache_type.': html retrieved from transient '.$cache_id );
+					return $wpsso->debug->get_html().$html;
+				}
+			}
+			$html = '<!-- '.$wpsso->cf['lca'].' sharing buttons begin -->' .
+				$wpsso->sharing->get_js( 'sharing-buttons-header', $ids ) .
+				$wpsso->sharing->get_html( $ids, $atts ) .
+				$wpsso->sharing->get_js( 'sharing-buttons-footer', $ids ) .
+				'<!-- '.$wpsso->cf['lca'].' sharing buttons end -->';
+	
+			if ( $wpsso->is_avail['cache']['transient'] ) {
+				set_transient( $cache_id, $html, $wpsso->cache->object_expire );
+				$wpsso->debug->log( $cache_type.': html saved to transient '.$cache_id.' ('.$wpsso->cache->object_expire.' seconds)');
+			}
+		} else $html = '<!-- '.$wpsso->cf['lca'].' sharing sharing buttons disabled -->';
+		return $wpsso->debug->get_html().$html;
+	}
+}
+
+?>
