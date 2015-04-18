1b6d0933152a2cdfee6b238442fa80527fde5bc6
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-18 16:49:00 +0000

    v1.3 tested up to wp 4.2 - simplify apikeys tab options
    git-svn-id: https://plugins.svn.wordpress.org/wpsso-ssb/tags/1.3@1138082 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/gpl/admin/apikeys.php
+++ lib/gpl/admin/apikeys.php
@@ -33,23 +33,19 @@ if ( ! class_exists( 'WpssoSsbGplAdminApikeys' ) ) {
 			if ( ! isset( $rows['plugin_min_shorten'] ) )
 				$rows['plugin_min_shorten'] = '<tr class="hide_in_basic">'.
 				$this->p->util->th( 'Minimum URL Length to Shorten', null, 'plugin_min_shorten' ). 
-				'<td class="blank">'.$form->get_hidden( 'plugin_min_shorten' ).
-					$this->p->options['plugin_min_shorten'].' characters</td>';
+				'<td class="blank">'.$this->p->options['plugin_min_shorten'].' characters</td>';
 
 			if ( ! isset( $rows['plugin_bitly_login'] ) )
 				$rows['plugin_bitly_login'] = $this->p->util->th( 'Bit.ly Username', null, 'plugin_bitly_login' ).
-				'<td class="blank mono">'.$form->get_hidden( 'plugin_bitly_login' ).
-					$this->p->options['plugin_bitly_login'].'</td>';
+				'<td class="blank mono">'.$this->p->options['plugin_bitly_login'].'</td>';
 
 			if ( ! isset( $rows['plugin_bitly_api_key'] ) )
 				$rows['plugin_bitly_api_key'] = $this->p->util->th( 'Bit.ly API Key', null, 'plugin_bitly_api_key' ).
-				'<td class="blank mono">'.$form->get_hidden( 'plugin_bitly_api_key' ).
-					$this->p->options['plugin_bitly_api_key'].'</td>';
+				'<td class="blank mono">'.$this->p->options['plugin_bitly_api_key'].'</td>';
 
 			if ( ! isset( $rows['plugin_google_api_key'] ) )
 				$rows['plugin_google_api_key'] = $this->p->util->th( 'Google Project App BrowserKey', null, 'plugin_google_api_key' ).
-				'<td class="blank mono">'.$form->get_hidden( 'plugin_google_api_key' ).
-					$this->p->options['plugin_google_api_key'].'</td>';
+				'<td class="blank mono">'.$this->p->options['plugin_google_api_key'].'</td>';
 
 			if ( ! isset( $rows['plugin_google_shorten'] ) )
 				$rows['plugin_google_shorten'] = $this->p->util->th( 'Google URL Shortener API is ON', null, 'plugin_google_shorten' ).
